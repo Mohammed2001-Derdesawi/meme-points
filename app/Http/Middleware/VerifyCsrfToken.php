6b5b7c24e07c6581f756a33c56f376db2880d318
Mohammed2001-Derdesawi <85463665+Mohammed2001-Derdesawi@users.noreply.github.com>
2022-07-29 14:27:40 +0300

    meme-points

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'https://meme-points.herokuapp.com/',
-        'https://meme-points.herokuapp.com/*',
+        'https://meme-points.herokuapp.com/points',
     ];
 }
